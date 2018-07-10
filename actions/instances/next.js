103144facee426db57f37fd2b24ef6975cdd795b
Julien Sarazin <julien.sarazin@me.com>
2018-07-10 15:54:55 +0200

    updated next()

--- actions/instances/next.js
+++ actions/instances/next.js
@@ -3,25 +3,15 @@ const Action = require('idylle').Action;
 module.exports = app => {
     const Instance = app.models.Instance;
 
-    class ServiceUnavailableError extends Error {
-        constructor(code, message) {
-            super(message);
-            this.code = code;
-        }
-    }
-
     return Action({
         rules: [],
         execute: (context) => {
-            const socket = context.socket;
-
             return Instance
                 .findOne()
                 .where(context.criteria.where)
                 .sort('updated_at')
-                .then(i => i || new ServiceUnavailableError(503, `service.${query.realm}.unavailable`))
-                .then(update)
-                .then(instance => socket.emit('YEMMA_INSTANCE', instance.toJSON()));
+                .then(i => i || context.error(503, `service.${context.criteria.where.realm}.unavailable`))
+                .then(update);
 
             function update(instance) {
                 return instance.save();
