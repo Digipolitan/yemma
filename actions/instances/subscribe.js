70212fd53b8afd443e8895ba308af78e3b32c6c0
Julien Sarazin <julien.sarazin@me.com>
2018-07-10 17:12:54 +0200

    updated token generation for registering servcies

--- actions/instances/subscribe.js
+++ actions/instances/subscribe.js
@@ -1,26 +1,36 @@
 const Action = require('idylle').Action;
+const uuid = require('uuid').v4;
 
-module.exports = app => Action({
-    rules: [
-        context => (context.data.realm && context.data.port && context.data.access_token) || context.error(400, 'missing.data')
-    ],
-    execute: (context) => {
-        const Instance = app.models.Instance;
-        const address = context.data.address || context.socket.handshake.address;
-        const port = context.data.port;
-        const uid = context.socket.id;
+module.exports = app => {
 
-        return Instance
-            .update(
-                { address, port },
-                {
-                    uid, address, port,
-                    realm: context.data.realm,
-                    timeout: context.data.timeout || app.settings.dispatch_timeout,
-                    access_token: context.data.access_token
-                },
-                {
-                    upsert: true
-                });
-    }
-});
+    return Action({
+        rules: [
+            context => (context.data.realm && context.data.port && context.data.access_token) || context.error(400, 'missing.data')
+        ],
+        execute: (context) => {
+            const Instance = app.models.Instance;
+            const address = context.data.address || context.socket.handshake.address;
+            const port = context.data.port;
+            const uid = context.socket.id;
+            const token = uuid();
+
+            return updateOrCreateInstance()
+                .then(() => token);
+
+            function updateOrCreateInstance() {
+                return Instance
+                    .update(
+                        { address, port },
+                        {
+                            uid, address, port,
+                            realm: context.data.realm,
+                            timeout: context.data.timeout || app.settings.dispatch_timeout,
+                            access_token: token
+                        },
+                        {
+                            upsert: true
+                        });
+            }
+        }
+    });
+}
